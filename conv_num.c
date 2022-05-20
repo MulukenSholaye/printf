39339e7da693edb7f894aa81b0abac7f7dfb6c60
MulukenSholaye <muluken@alx.com>
2022-05-20 13:25:02 -0700

    changes

--- conv_num.c
+++ conv_num.c
@@ -13,7 +13,7 @@ int print_hex(va_list ap, params_t *params)
 	int z = 0;
 	char *str1;
 
-	if (params->lo_modifier)
+	if (params->l_modifier)
 		lo = (unsigned long)va_arg(ap, unsigned long);
 	else if (params->h_modifier)
 		lo = (unsigned short int)va_arg(ap, unsigned int);
@@ -43,7 +43,7 @@ int print_HEX(va_list ap, params_t *params)
 	int b = 0;
 	char *str11;
 
-	if (params->dl_modifier)
+	if (params->l_modifier)
 		dl = (unsigned long)va_arg(ap, unsigned long);
 	else if (params->h_modifier)
 		dl = (unsigned short int)va_arg(ap, unsigned int);
